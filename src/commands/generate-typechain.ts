e1a307990f6e1b7319b211f8771030a28e480fe7
cryptickoan <me@cryptickoan.com>
2022-06-26 02:01:36 -0600

    mvp

---
+++ src/commands/generate-typechain.ts
@@ -0,0 +1,46 @@
+import chalk from "chalk"
+import { execSync } from "child_process"
+import { Command } from "./extract"
+
+const generateTypechainContracts = (pair: string[]) => {
+    // 1. Call typechain to generate typed contracts
+    const output = execSync('npx typechain --target ethers-v5 --out-dir ' +  pair[1] + " " + pair[0],{ encoding: 'utf-8' })
+    console.log(chalk.green(output.slice(0, output.indexOf('!')) + " for pair " + pair))
+}
+
+const generateTypechain = (argv: any) => {
+    // Get all pairs
+    const givenArguments = argv['pairs']
+    
+    // For every pair generate typechain contracts
+    let i = 0
+    while (i < givenArguments.length) {
+        const array = [givenArguments[i], givenArguments[i + 1]]
+        generateTypechainContracts(array)
+        i = i + 2
+    }
+}
+
+const generateTypechainCheck = (argv: any) => {
+        // @ts-ignore
+       if (argv['pairs'] && argv['pairs'].length < 2) {
+             throw new Error(chalk.red("Pairs is empty, please provide at least one pair after --pairs."))
+       }
+
+       return true
+}
+
+export const generateTypechainCommand: Command = {
+    name: "generate-typechain",
+    description: "Generate typechain for the given contract and save output to the fiven path",
+    options: {
+        pairs: {
+              "alias": "-p",
+              "describe": "Contract source path and typechain contract output path",
+              "type": "array",
+              "demandOption": true
+        }
+    },
+    function: async function (argv: any) { generateTypechain(argv) },
+    check: generateTypechainCheck
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs'
+import path from 'path';
+
+export const readAndFilterDirectory = (
+    path: string,
+    filter?: string[]
+) => {
+    // 1. Load all files in the directory.
+    let filesInDirectory: string[]
+    try {
+        filesInDirectory = fs.readdirSync(path)
+    } catch (e) {
+        throw Error('fuck me :)')
+    }
+    
+    // 2. If theres a filter, filter the filesInDirectory array.
+    if (filter) {
+        for (const i of filter) {
+            filesInDirectory  = filesInDirectory.filter(file => !file.includes(i))
+        }
+    }
+
+    return filesInDirectory
+}
+
+export function loadJson(path: string): any {
+    return JSON.parse(fs.readFileSync(path).toString());
+}
+
+export function atomicWrite(targetPath: string, value: string | Uint8Array): void {
+    const tmp = path.resolve(__dirname, ".atomic-tmp");
+    fs.writeFileSync(tmp, value);
+    fs.renameSync(tmp, targetPath);
+}
+
+export function saveJson(filename: string, data: any, sort?: boolean): any {
+    atomicWrite(filename, JSON.stringify(data) + "\n");
+}
